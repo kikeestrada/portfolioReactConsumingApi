f00ec7e7fc335db8a2d92e604aa62c04f1f9ddb5
Enrique Jose Estrada Nunez <kikeestrada@gmail.com>
2019-12-09 13:51:32 -0600

    ajustes

--- src/components/AppRoutes.jsx
+++ src/components/AppRoutes.jsx
@@ -1,9 +1,9 @@
 import React from "react"
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
-import Mainmenu from './organisms/MainMenu'
-import Banner from './organisms/Banner'
 import Home from "../components/pages/Home"
 import Episodes from "../components/pages/Episodes"
+import Mainmenu from './organisms/MainMenu'
+import Banner from './organisms/Banner'
 
 const AppRoutes = () => (
     <Router>
@@ -12,11 +12,11 @@ const AppRoutes = () => (
         <Switch>
             <Route path = "/" exact component={Home}/>
             <Route path = "/Episodes" exact component={Episodes}/>
-            <Route component={() => (
-                <div className="ed-grid">
-                    <h1>Página no encontrada</h1>
-                </div>
-            )}/>
+            {/*<Route component={() => (*/}
+                {/*<div className="ed-grid">*/}
+                    {/*<h1>Página no encontrada</h1>*/}
+                {/*</div>*/}
+            {/*)}/>*/}
         </Switch>
     </Router>
 
