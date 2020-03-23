87820475f4cd4340c1c44dd760ee99f619214bf0
kikeestrada <kikeestrada@gmail.com>
2020-03-23 12:38:07 -0600

    Cambios

--- src/components/AppRoutes.jsx
+++ src/components/AppRoutes.jsx
@@ -4,15 +4,16 @@ import MainMenu from './organisms/MainMenu'
 import Banner from './organisms/Banner'
 import Home from "../components/pages/Home"
 import Episodes from "../components/pages/Episodes"
+// import App from './App';
 
 const AppRoutes = () => (
     <Router>
         <MainMenu/>
         <Banner/>
         <Switch>
-            <Route path ="/" exact component={Home}/>
+            <Route exact path ="/"  component={Home}/>
             <Route path ="/Episodes" component={Episodes}/>
-            <Route component={() => (
+            <Route path="*" component={() => (
                 <div className="ed-grid">
                     <h1>Página no encontrada</h1>
                 </div>
