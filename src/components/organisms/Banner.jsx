a6941e4cd1e7b6718ebfc9187ed56a013941cdb7
kikeestrada <kikeestrada@gmail.com>
2020-03-26 14:43:10 -0600

    update

--- src/components/organisms/Banner.jsx
+++ src/components/organisms/Banner.jsx
@@ -6,7 +6,7 @@ const Banner = () =>(
             <div className="lg-cols-4 lg-x-2">
                 <img className="main-banner__img" src="img/banner-img1.png" alt="alt-Banner"/>
                 <div className="main-banner__data s-center">
-                    <p className="t2 s-mb-0">Gamanza 2020</p>
+                    <p className="t2 s-mb-0">Consuming an API</p>
                     <p>The iGaming Super Store</p>
                     <input className="nav-input" type="text" placeholder="Explore Products"/>
                 </div>
--- src/components/organisms/MainMenu.jsx
+++ src/components/organisms/MainMenu.jsx
@@ -8,7 +8,7 @@ const MainMenu = () => (
     <header className="main-header">
         <div className="ed-grid s-grid-5 lg-grid-4">
             <div className="s-cols-4 lg-cols-1 s-cross-center">
-                <img className="main-logo" src="img/logo.png" alt="logo" />
+                {/*<img className="main-logo" src="img/logo.png" alt="logo" />*/}
             </div>
             <div className="s-grid-1 lg-cols-3 s-cross-center s-main-end header-links">
                 <nav className="main-menu" ref={menu}>
